394e00b8201ede6619ad68a1254552be4b918df6
Matei-Marius Micu <micumatei@gmail.com>
2019-08-31 19:28:47 +0300

    ci(setup): trigger publish

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ README = open(
 # This call to setup() does all the work
 setup(
     name='auto-tag',
-    version='0.3.0',
+    version='0.3.1',
     description='Automatically tag a branch based on commit message',
     long_description=README,
     long_description_content_type='text/markdown',
